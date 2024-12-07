6c946d36ef666d75fb72452eef18fb39f169a451
DianaHosh <146379540+DianaHosh@users.noreply.github.com>
2024-12-08 00:11:26 +0200

    Update wsgi.py

--- fitnesssite/wsgi.py
+++ fitnesssite/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+sys.path.append('/home/site/wwwroot')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitnesssite.settings')
