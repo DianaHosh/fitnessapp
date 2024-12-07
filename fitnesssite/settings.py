2803ad58564812bb5c4cee97808fa1f499e9f122
DianaHosh <146379540+DianaHosh@users.noreply.github.com>
2024-12-08 00:19:52 +0200

    Update settings.py

--- fitnesssite/settings.py
+++ fitnesssite/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = 'django-insecure-8--5u6=l8z=j28^a%)5t$1=$^7yv(wvydr1&p$+9-=huwv0q!c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['good-fitness-app.azurewebsites.net',
+                 'localhost',
+                 '127.0.0.1']
 
 
 # Application definition
